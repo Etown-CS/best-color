36bcc43e99d5c436330751e6cac9a4dbf519a631
wheelsbot7 <o.wertzberger@gmail.com>
2026-01-20 11:21:22 -0500

    implement basic database schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,3 +1,10 @@
-import { pgTable, serial, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp, integer } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', { id: serial('id').primaryKey(), age: integer('age') });
+export const votes = pgTable('votes', {
+	id: serial('id').primaryKey(),
+	color1: text('color1').notNull(),
+	color2: text('color2').notNull(),
+	chosenColor: text('chosen_color').notNull(),
+	userAgent: text('user_agent'),
+	createdAt: timestamp('created_at').defaultNow().notNull()
+});
