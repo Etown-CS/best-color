2e9c74f8f188476d7a48170b869e460599263bd4
wheelsbot7 <o.wertzberger@gmail.com>
2026-02-11 17:57:24 -0500

    simplify ui display and add dark mode button

--- src/lib/components/ui.svelte
+++ src/lib/components/ui.svelte
@@ -1,19 +1,35 @@
 <script>
 	export let name = 'ui1';
 	export let name2 = 'radio';
+	export let pagination = 'page';
 </script>
 
-<div class="tabs tabs-box">
-	<input type="radio" {name} class="tab" checked="checked" aria-label="Inputs" />
-	<div class="tab-content bg-base-100 border-base-300 p-6">
+<div class="box">
+	<div class="bg-base-100 border-base-300 p-6">
 		<button class="btn btn-primary">Button</button>
 		<input type="checkbox" checked="checked" class="toggle toggle-primary" />
 		<input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
 		<input type="radio" name={name2} class="radio radio-primary" checked="checked" />
 		<input type="radio" name={name2} class="radio radio-primary" />
 	</div>
-	<input type="radio" {name} class="tab" aria-label="Tab 2" />
-	<div class="tab-content bg-base-100 border-base-300 p-6">Tab content 2</div>
-	<input type="radio" {name} class="tab" aria-label="Tab 3" />
-	<div class="tab-content bg-base-100 border-base-300 p-6">Tab content 3</div>
+	<div class="bg-base-100 border-base-300 p-6">
+		<div class="join">
+			<input
+				class="join-item btn btn-square"
+				type="radio"
+				name={pagination}
+				aria-label="1"
+				checked="checked"
+			/>
+			<input class="join-item btn btn-square" type="radio" name={pagination} aria-label="2" />
+			<input class="join-item btn btn-square" type="radio" name={pagination} aria-label="3" />
+			<input class="join-item btn btn-square" type="radio" name={pagination} aria-label="4" />
+		</div>
+	</div>
+	<div class="bg-base-100 border-base-300 p-6">
+		<div class="indicator">
+			<span class="indicator-item badge badge-primary">New</span>
+			<div class="bg-base-300 grid h-12 w-22 place-items-center">content</div>
+		</div>
+	</div>
 </div>
